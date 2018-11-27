8147cef78b8ce967b0960679c84f2dec43950232
mEkenstein <mattiasekstrom88@gmail.com>
2018-11-27 19:25:50 +0100

    Some additional commenting

--- pitchDetector/myFFT.cpp
+++ pitchDetector/myFFT.cpp
@@ -12,8 +12,10 @@ int findPeak(kiss_fft_cpx *fftData, int size) {
 	float maxVal = 0.0;
 	float curVal = 0.0;
 	int peakInd = 0;
-	int minInd = 10;
-	for (int ind = minInd; ind < size; ind++) {
+	int minInd = 10;			// Då FFTn inte har modifierats med en fönsterfunktion struntar vi i de tio lägsta och högsta frekvensbinsen.
+	int maxInd = size - 10;		// Så länge vi jobbar med hyfsat periodiska signaler (tex från gitarrsträngar) så är det dock ingen fara, 
+								// då problem vid kanterna uppstår för signaler som är icke-periodiska.	
+	for (int ind = minInd; ind < maxInd; ind++) {
 		curVal = fftData[ind].r * fftData[ind].r + fftData[ind].i * fftData[ind].i;
 		if (curVal > maxVal) {
 			maxVal = curVal;
